ed5a6133fb8b6eb19a3ab5be82b2bf725e1e5924
Dane Middleton <middletondane@gmail.com>
2018-08-12 11:43:17 -0500

    complete Part 1 - Server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 let express = require("express");
-let bodyParser = require("body-parser")
+let bodyParser = require("body-parser");
 let comments = require("./comments");
 let products = require("./products");
 let vehicles = require("./vehicles");
 let contacts = require("./contacts");
 
 const app = express();
+
+app.use(bodyParser.json())
+
 
 app.get("/contacts",function (request,response){
     response.json(contacts)
@@ -23,30 +26,64 @@ app.get("/products",function (request,response){
     response.json(products)
    });
 
-//    request.params.id
+app.get("/contact/:id",function (request,response){
+    let foundContact = contacts.find(function(item) {
+        return item._id == request.params.id;
+ });
+response.json(foundContact)
+});
 
-
-app.get("/contacts/:id",function (request,response){
-    response.json(contacts)
+app.get("/vehicle/:id",function (request,response){
+    let foundVehicle = vehicles.find(function(item) {
+        return item._id == request.params.id;
+    });
+   response.json(foundVehicle)
    });
 
-app.get("/vehicles/:id",function (request,response){
-    response.json(vehicles)
+app.get("/comment/:id",function (request,response){
+    let foundComment = comments.find(function(item) {
+        return item._id == request.params.id;
+    });
+   response.json(foundComment)
    });
 
-app.get("/comments/:id",function (request,response){
-    response.json(comments)
+app.get("/product/:id",function (request,response){
+    let foundProduct = products.find(function(item) {
+        return item._id == request.params.id;
+    });
+   response.json(foundProduct)
    });
 
-app.get("/products/:id",function (request,response){
-    response.json(products)
+
+app.post("/contacts/",function (request,response){
+    request.body._id = contacts.length + 1;
+    let newContact = request.body;
+    contacts.push(newContact);
+    response.json(newContact);
    });
 
-//    ### Create express routes to get one thing
-//    * Add .get() routes for /contacts/:id, /vehicles/:id, /comments/:id, /products/:id
-//    * add a path variable for id
-//    * use the params.id to .find() the item from the appropriate array
-//    * response.json() the item found 
+app.post("/vehicles/",function (request,response){
+    request.body._id = vehicles.length + 1;
+    let newVehicle = request.body;
+    vehicles.push(newVehicle);
+    response.json(newVehicle);
+   });
+ 
+app.post("/comments/",function (request,response){
+    request.body._id = comments.length + 1;
+    let newComment = request.body;
+    comments.push(newComment)
+    response.json(newComment)
+   });
+
+app.post("/products/",function (request,response){
+    request.body._id = products.length + 1;
+    let newProduct = request.body;
+    products.push(newProduct)
+    response.json(newProduct)
+   });
+
+
 
 app.listen(3001, (err) => {
 if (err) {
